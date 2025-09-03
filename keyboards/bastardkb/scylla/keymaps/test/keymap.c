661f88d72724411e1a7f7de103f557606635adda
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-09-03 16:51:24 -0400

    got rid of all grid and spacing in the layout

--- keyboards/bastardkb/scylla/keymaps/test/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/test/keymap.c
@@ -23,19 +23,6 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [test] = LAYOUT_split_4x6_5(
-    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_DQUO,
-    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-                                   KC_LGUI, KC_LALT, MO(top),    MO(front),  KC_SPC, KC_BSPC,
-    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-                                           MO(swap), MO(ultra),     KC_ESC,  KC_ENT
-    ),
+    [test] = LAYOUT_split_4x6_5( XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_DQUO, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LGUI, KC_LALT, MO(top), MO(front), KC_SPC, KC_BSPC, MO(swap), MO(ultra), KC_ESC, KC_ENT),
 
 };
