1226afe267879d097be4ff49c9518a2ddc8a20d3
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-09-03 17:32:34 -0400

    re-added MO(top)

--- keyboards/bastardkb/scylla/keymaps/sskeymap/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/sskeymap/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
     //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-                                   KC_LGUI, KC_LALT, XXXXXXX,    XXXXXXX,  KC_SPC, KC_BSPC,
+                                   KC_LGUI, KC_LALT, MO(top),    XXXXXXX,  KC_SPC, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
                                            XXXXXXX, XXXXXXX,     KC_ESC,  KC_ENT
     ),
