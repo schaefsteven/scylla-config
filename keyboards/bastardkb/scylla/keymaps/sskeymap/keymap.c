48e275faf004cd7344b3f3236449c5450a01566b
schaefsteven <66270901+schaefsteven@users.noreply.github.com>
2025-10-05 12:35:13 -0400

    added gmain layer

--- keyboards/bastardkb/scylla/keymaps/sskeymap/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/sskeymap/keymap.c
@@ -105,6 +105,7 @@ enum layer_names {
     ultra,
     gaming,
     gtop,
+    gmain,
 };
 
 enum unicode_names {
@@ -234,7 +235,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
                                    KC_LALT,  KC_SPC, MO(gtop),   MO(front),  KC_SPC, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
-                                            KC_RCTL, MO(main),      KC_ESC,  KC_ENT
+                                            KC_RCTL, MO(gmain),     KC_ESC,  KC_ENT
     ),
 
     [gtop] = LAYOUT_split_4x6_5(
@@ -252,4 +253,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______,      XXXXXXX, XXXXXXX
     ),
 
+    [gmain] = LAYOUT_split_4x6_5(
+    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
+         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_DQUO,
+    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
+        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
+                      KC_LGUI, MT(MOD_LALT, KC_SPC), MO(top),    MO(front),  KC_SPC, KC_BSPC,
+    //|--------+--------+--------+--------+--------+--------|    |--------+--------+--------+--------+--------+--------|
+                                           MO(swap), _______,     KC_ESC,  KC_ENT
+    ),
 };
